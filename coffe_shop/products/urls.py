014625af95060f7977451d51deae0e93e9e56c50
soyandrestrujillo <andymania.1301@gmail.com>
2024-10-26 00:43:55 -0500

    feat: get product list

--- coffe_shop/products/urls.py
+++ coffe_shop/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductList
 
 urlpatterns = [
     path("add/", ProductListView.as_view(), name="add_product"),
+    path("list/", ProductList.as_view(), name="list_product"),
 ]
--- coffe_shop/products/views.py
+++ coffe_shop/products/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from .forms import ProductForm
+from .models import Product
 
 # Create your views here.
 
@@ -9,8 +10,13 @@ from .forms import ProductForm
 class ProductListView(generic.FormView):
     template_name = "products/add_product.html"
     form_class = ProductForm
-    success_url = reverse_lazy("add_product")
+    success_url = reverse_lazy("list_product")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class ProductList(generic.ListView):
+    model = Product
+    template_name = "products/list_product.html"
