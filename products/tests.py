28ecafeacfec4f7e6313791b63146d57321263d1
soyandrestrujillo <andymania.1301@gmail.com>
2024-10-29 20:04:03 -0500

    feat: testting

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from products.models import Product
 
 # Create your tests here.
+
+
+class ProductListViewTests(TestCase):
+    def test_should_return_200(self):
+        url = reverse("list_product")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_return_200_with_products(self):
+        url = reverse("list_product")
+        Product.objects.create(
+            name="Test Product",
+            description="Test Description",
+            price=10.00,
+            available=True,
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["products"]), 1)
--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("add/", ProductAddView.as_view(), name="add_product"),
     path("", ProductListView.as_view(), name="list_product"),
+    path("add/", ProductAddView.as_view(), name="add_product"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
